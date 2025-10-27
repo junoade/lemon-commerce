bd8ca12108f8071efe724b8703035d9fd48e4f5c
junoade <ajchoi0928@gmail.com>
2025-10-27 17:26:15 +0900

    test: 회원가입 단위테스트 코드 추가
    `UserModel 생성 테스트`
    
    @Nested 를 활용한 회원 객체 생성에 관련된 @Test 시나리오 묶음

--- apps/commerce-api/src/test/java/com/loopers/domain/user/UserModelTest.java
+++ apps/commerce-api/src/test/java/com/loopers/domain/user/UserModelTest.java
@@ -2,6 +2,7 @@ package com.loopers.domain.user;
 
 import com.loopers.support.error.CoreException;
 import com.loopers.support.error.ErrorType;
+import jakarta.persistence.Column;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,55 +12,218 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UserModelTest {
-    @DisplayName("예시 모델을 생성할 때, ")
+    @DisplayName("UserModel 생성 테스트")
     @Nested
     class Create {
-        @DisplayName("제목과 설명이 모두 주어지면, 정상적으로 생성된다.")
+        @DisplayName("이용자ID, 이메일, 생년월일, 이용자이름, 성별 입력값이 유효하면 User 객체 생성에 성공한다")
         @Test
-        void createsExampleModel_whenNameAndDescriptionAreProvided() {
+        void createsExampleModel_whenAllRequiredFieldsAreProvidedAndValid() {
             // arrange
-            String name = "제목";
-            String description = "설명";
+            String userId = "ajchoi0928";
+            String userName = "junho";
+            String description = "loopers backend developer";
+            String email = "loopers@loopers.com";
+            String birthDate = "1997-09-28";
+            String gender = "M";
 
             // act
-            UserModel userModel = new UserModel(name, description);
+            UserModel userModel = new UserModel(userId, userName, description, email, birthDate, gender);
 
             // assert
             assertAll(
-                () -> assertThat(userModel.getId()).isNotNull(),
-                () -> assertThat(userModel.getName()).isEqualTo(name),
-                () -> assertThat(userModel.getDescription()).isEqualTo(description)
+                    () -> assertThat(userModel.getId()).isNotNull(),
+                    () -> assertThat(userModel.getUserName()).isEqualTo(userName),
+                    () -> assertThat(userModel.getDescription()).isEqualTo(description),
+                    () -> assertThat(userModel.getDescription()).isEqualTo(description),
+                    () -> assertThat(userModel.getEmail()).isEqualTo(email),
+                    () -> assertThat(userModel.getBirthDate()).isEqualTo(birthDate),
+                    () -> assertThat(userModel.getGender()).isEqualTo(gender),
+                    () -> assertThat(userModel.getPoint()).isEqualTo(0)
             );
         }
 
-        @DisplayName("제목이 빈칸으로만 이루어져 있으면, BAD_REQUEST 예외가 발생한다.")
+        @DisplayName("이용자ID가 10자를 초과했을 때 BAD_REQUEST를 반환한다")
         @Test
-        void throwsBadRequestException_whenTitleIsBlank() {
+        void throwsBadRequestException_whenUserIdLengthOverThanExpected() {
             // arrange
-            String name = "   ";
+            // arrange
+            String userId = "ajchoi12345";
+            String userName = "junho";
+            String description = "loopers backend developer";
+            String email = "loopers@loopers.com";
+            String birthDate = "1997-09-28";
+            String gender = "M";
 
             // act
             CoreException result = assertThrows(CoreException.class, () -> {
-                new UserModel(name, "설명");
+                new UserModel(userId, userName, description, email, birthDate, gender);
             });
 
             // assert
             assertThat(result.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
         }
 
-        @DisplayName("설명이 비어있으면, BAD_REQUEST 예외가 발생한다.")
+        @DisplayName("이용자ID가 영문 또는 숫자 외 문자를 포함할 때 BAD_REQUEST를 반환한다")
         @Test
-        void throwsBadRequestException_whenDescriptionIsEmpty() {
+        void throwsBadRequestException_whenUserIdContainsInvalidCharacters() {
             // arrange
-            String description = "";
+            // arrange
+            String userId = "ajchoi_97";
+            String userName = "junho";
+            String description = "loopers backend developer";
+            String email = "loopers@loopers.com";
+            String birthDate = "1997-09-28";
+            String gender = "M";
 
             // act
             CoreException result = assertThrows(CoreException.class, () -> {
-                new UserModel("제목", description);
+                new UserModel(userId, userName, description, email, birthDate, gender);
             });
 
             // assert
             assertThat(result.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
         }
+
+        @DisplayName("이용자ID가 빈 경우 BAD_REQUEST를 반환한다")
+        @Test
+        void throwsBadRequestException_whenUserIdNotValid() {
+            // arrange
+            String userId = " ";
+            String userName = "junho";
+            String description = "loopers backend developer";
+            String email = "loopers@loopers.com";
+            String birthDate = "1997-09-28";
+            String gender = "M";
+
+            // act
+            CoreException result = assertThrows(CoreException.class, () -> {
+                new UserModel(userId, userName, description, email, birthDate, gender);
+            });
+
+            // assert
+            assertThat(result.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+        }
+
+        @DisplayName("이메일 형식이 xx@yy.zz에 맞지 않으면 BAD_REQUEST를 반환한다")
+        @Test
+        void throwsBadRequestException_whenEmailNotValid() {
+            // arrange
+            String userId = "ajchoi0928";
+            String userName = "junho";
+            String description = "loopers backend developer";
+            String email = "xy@looperscom";
+            String birthDate = "1997-09-28";
+            String gender = "M";
+
+            // act
+            CoreException result = assertThrows(CoreException.class, () -> {
+                new UserModel(userId, userName, description, email, birthDate, gender);
+            });
+
+            // assert
+            assertThat(result.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+        }
+
+        @DisplayName("이메일이 빈 경우 BAD_REQUEST를 반환한다")
+        @Test
+        void throwsBadRequestException_whenEmailIsBlank() {
+            // arrange
+            String userId = "ajchoi0928";
+            String userName = "junho";
+            String description = "loopers backend developer";
+            String email = "";
+            String birthDate = "1997-09-28";
+            String gender = "M";
+
+            // act
+            CoreException result = assertThrows(CoreException.class, () -> {
+                new UserModel(userId, userName, description, email, birthDate, gender);
+            });
+
+            // assert
+            assertThat(result.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+        }
+
+        @DisplayName("생년월일이 yyyy-MM-dd 포맷이 아니면 BAD_REQUEST를 반환한다")
+        @Test
+        void throwsBadRequestException_whenBirthDateNotValid() {
+            // arrange
+            String userId = "ajchoi0928";
+            String userName = "junho";
+            String description = "loopers backend developer";
+            String email = "ajchoi0928@loopers.com";
+            String birthDate = "1997/09/28";
+            String gender = "M";
+
+            // act
+            CoreException result = assertThrows(CoreException.class, () -> {
+                new UserModel(userId, userName, description, email, birthDate, gender);
+            });
+
+            // assert
+            assertThat(result.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+        }
+
+        @DisplayName("생년월일이 빈 경우 BAD_REQUEST를 반환한다")
+        @Test
+        void throwsBadRequestException_whenBirthDateIsBlank() {
+            // arrange
+            String userId = "ajchoi0928";
+            String userName = "junho";
+            String description = "loopers backend developer";
+            String email = "ajchoi0928@loopers.com";
+            String birthDate = "";
+            String gender = "M";
+
+            // act
+            CoreException result = assertThrows(CoreException.class, () -> {
+                new UserModel(userId, userName, description, email, birthDate, gender);
+            });
+
+            // assert
+            assertThat(result.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+        }
+        // user_name, 성별,
+        @DisplayName("회원 이름이 빈 경우 BAD_REQUEST를 반환한다")
+        @Test
+        void throwsBadRequestException_whenUserNameIsBlank() {
+            // arrange
+            String userId = "ajchoi0928";
+            String userName = "";
+            String description = "loopers backend developer";
+            String email = "ajchoi0928@loopers.com";
+            String birthDate = "1997-09-28";
+            String gender = "M";
+
+            // act
+            CoreException result = assertThrows(CoreException.class, () -> {
+                new UserModel(userId, userName, description, email, birthDate, gender);
+            });
+
+            // assert
+            assertThat(result.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+        }
+
+        @DisplayName("성별이 빈 경우 BAD_REQUEST를 반환한다")
+        @Test
+        void throwsBadRequestException_whenGenderIsBlank() {
+            // arrange
+            String userId = "ajchoi0928";
+            String userName = "최준호";
+            String description = "loopers backend developer";
+            String email = "ajchoi0928@loopers.com";
+            String birthDate = "1997-09-28";
+            String gender = "";
+
+            // act
+            CoreException result = assertThrows(CoreException.class, () -> {
+                new UserModel(userId, userName, description, email, birthDate, gender);
+            });
+
+            // assert
+            assertThat(result.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+        }
+
+
     }
 }
