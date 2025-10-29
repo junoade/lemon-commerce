d4c183272964d08b22f043391b396a2b56563103
JUNHO CHOI <54317409+junoade@users.noreply.github.com>
2025-10-30 02:02:16 +0900

    Revert "Week1 회원가입 관련 통합테스트 구현"

--- apps/commerce-api/src/main/java/com/loopers/domain/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.loopers.domain.user;
-
-import java.util.Optional;
-
-public interface UserRepository {
-    boolean existsUserId(String userId);
-    Optional<UserModel> findByUserId(String userId);
-    UserModel save(UserModel user);
-}
